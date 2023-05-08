91f639127a67b6608fc2bb99413ba3b87652d59d
Luis.Fernando <luisfmiranda8@gmail.com>
2023-05-08 01:01:37 -0500

    summary function as param

--- pkg/utils/util.go
+++ pkg/utils/util.go
@@ -13,7 +13,7 @@ import (
 	"gonum.org/v1/gonum/graph/simple"
 )
 
-func NewFunctionIndexer(directory string) ([]funcnode.FuncNode, error) {
+func NewFunctionIndexer(directory string, summaryFuntion func(string, []string) string) ([]funcnode.FuncNode, error) {
 	parser := sitter.NewParser()
 	parser.SetLanguage(golang.GetLanguage())
 
@@ -57,9 +57,32 @@ func NewFunctionIndexer(directory string) ([]funcnode.FuncNode, error) {
 		if len(node.Body) == 0 {
 			continue
 		}
+		if node.Summary == "" {
+			getNodeSummary(node, g, summaryFuntion)
+		}
 		nodeList = append(nodeList, *node)
 		fmt.Printf("Function: %s\nCombined Body:\n%s\n\n", node.Name, node.Body)
 	}
 
 	return nodeList, nil
+}
+
+func getNodeSummary(node *funcnode.FuncNode, g *simple.DirectedGraph, summaryFuntion func(string, []string) string) string {
+	if node.Summary != "" {
+		return node.Summary
+	}
+
+	calledSummaries := []string{}
+
+	fromEdges := g.From(node.ID())
+	for fromEdges.Next() {
+		target := fromEdges.Node().(*funcnode.FuncNode)
+		if len(target.Body) > 0 {
+			calledSummaries = append(calledSummaries, getNodeSummary(target, g, summaryFuntion))
+		}
+
+	}
+
+	node.Summary = summaryFuntion(node.Body, calledSummaries)
+	return node.Summary
 }
