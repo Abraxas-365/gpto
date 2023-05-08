34a6d847d58ca6a6db8b5cace9d5f1f37e9f9d90
Luis.Fernando <luisfmiranda8@gmail.com>
2023-05-08 01:22:37 -0500

    creating summarisation function

---
+++ pkg/summary/summary.go
@@ -0,0 +1,38 @@
+package summary
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/tmc/langchaingo/exp/chains"
+)
+
+func Create(body string, calledFunctionsSummaries []string) string {
+	ctx := context.Background()
+	summaryChain, err := NewSummaryChain()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	refineSummaryChain, err := NewRefineSummaryChain()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	calledFunctionSummariesString := strings.Join(calledFunctionsSummaries, "\n")
+
+	summary, err := chains.Call(ctx, summaryChain, map[string]any{
+		"function": body,
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	refinedSummary, err := chains.Call(ctx, refineSummaryChain, map[string]any{
+		"function_summary":           summary,
+		"called_functions_summaries": calledFunctionSummariesString,
+	})
+
+	return refinedSummary["text"].(string)
+}
